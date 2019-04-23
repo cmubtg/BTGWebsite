1c3853d540442c741928344c3cde52df9eb8d6a3
Eric Cheng <ericcheng986@gmail.com>
2019-04-22 22:14:24 -0400

    Step 1. Polls testing

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,3 +1,4 @@
+from django.urls import include, path
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -17,5 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
